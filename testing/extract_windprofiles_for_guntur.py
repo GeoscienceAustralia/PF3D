1e597d3cbcdc1ef89b66fccb362990a18e244201
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2010-10-28 04:56:37 +0000

    Rationalised wind field extraction

--- testing/extract_windprofiles_for_guntur.py
+++ testing/extract_windprofiles_for_guntur.py
@@ -25,7 +25,22 @@ NCEP_dir = '/model_area/tephra/3D_wind/NCEP1/hazardmap'
 # Location of generated windprofiles
 windfield_directory = 'guntur_wind'
 
+# Determine if generated wind profile should be used for hazard modelling 
+# Options are 
+#    'multiple' (hazard modelling)
+#    'merged'   (scenario modelling)
+wind_field_type = 'merged'
+#wind_field_type = 'multiple'
+
 #--------------------------------------
 if __name__ == '__main__':
     from aim import generate_wind_profiles_from_ncep
-    generate_wind_profiles_from_ncep(__file__)
+    
+    if wind_field_type == 'multiple':
+        generate_wind_profiles_from_ncep(__file__)
+    elif wind_field_type == 'merged':
+        from aim import join_wind_profiles
+        generate_wind_profiles_from_ncep(__file__, update_timeblocks=True)
+        join_wind_profiles(windfield_directory)
+    else:
+        print 'wind_field_type must be either \'multiple\' or \'merged\'' 
